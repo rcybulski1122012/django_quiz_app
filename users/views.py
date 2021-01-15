4e20e5765201b5d689311bf48f12de4d1a56173b
Radoslaw <r.cybulski1122012@gmail.com>
2021-01-15 20:19:30 +0100

    Add success message to register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from users.forms import UserRegistrationForm
@@ -8,6 +9,7 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your account has been created successfully!', extra_tags='alert alert-success')
             return redirect('home')
     else:
         form = UserRegistrationForm()
