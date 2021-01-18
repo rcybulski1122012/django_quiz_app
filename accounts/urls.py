837c297570e9958be2fb3799808e20166c27de28
Radoslaw <r.cybulski1122012@gmail.com>
2021-01-18 06:51:59 +0100

    Create change password views
    change_password and change_password_done views were created.
    Also 'accounts' links on the navbar were moved to the right side.

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html'), name='password_change'),
+    path('password_change_done/,', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'), name='password_change_done')
 ]
